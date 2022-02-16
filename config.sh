8f5f94c114752c481000ddd3e53cd253efdc02b6
Bencece <zahoranbence@hotmail.com>
2022-02-16 21:17:17 +0100

    Edited docker file

--- config.sh
+++ config.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 
 echo "$(tput setaf 2)Do some configurations...$(tput sgr 0)"
-echo "Please provide your username to make necessary folders!"
+echo "Please provide your username to make necessary folders and configs!"
 
 read uname
+
+sudo groupadd docker
+
+sudo usermod -aG docker $uname
+
+newgrp docker
 
 mkdir /home/$uname/jellyfin
 mkdir /home/$uname/jellyfin/config
