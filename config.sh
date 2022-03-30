ff16429cbe3b44f17151576dfb8a597474bc17b9
Bencece <zahoranbence@hotmail.com>
2022-03-30 20:41:58 +0200

    Edited compose

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "$(tput setaf 2)Do some configurations...$(tput sgr 0)"
-echo "Please provide your username to make necessary folders and configs!"
+echo "Please provide your username to make necessary folders and configs! (like: ubuntu)"
 
 read uname
 
@@ -14,5 +14,11 @@ newgrp docker
 mkdir /home/$uname/jellyfin
 mkdir /home/$uname/jellyfin/config
 mkdir /home/$uname/jellyfin/cache
+
+mkdir -p /home/$uname/apache/config
+
+echo "Please provide your domain url to make the httpd config! (like: my-domain.awsome.net)"
+
+read url
 
 echo "$(tput setaf 2)We are done! Ready to start. Please run start.sh!$(tput sgr 0)"
