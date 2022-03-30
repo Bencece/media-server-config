8c4f00d402bcee34612f8e928c67dd8639bc50a0
Bencece <zahoranbence@hotmail.com>
2022-03-30 20:56:56 +0200

    Edited install.sh

--- install.sh
+++ install.sh
@@ -20,4 +20,29 @@ curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compo
 
 chmod +x ~/.docker/cli-plugins/docker-compose
 
-echo "(tput setaf 2)Installation is complete, please run the config.sh file!$(tput sgr 0)"
+echo "(tput setaf 2)Installation is complete!$(tput sgr 0)"
+
+echo "$(tput setaf 2)Do some configurations...$(tput sgr 0)"
+echo "Please provide your username to make necessary folders and configs! (like: ubuntu)"
+
+read uname
+
+sudo groupadd docker
+
+sudo usermod -aG docker $uname
+
+newgrp docker
+
+mkdir /home/$uname/jellyfin
+mkdir /home/$uname/jellyfin/config
+mkdir /home/$uname/jellyfin/cache
+
+mkdir -p /home/$uname/apache/config
+
+echo "Please provide your domain url to make the httpd config! (like: my-domain.awsome.net)"
+
+read url
+
+sed -i "s/!!url!!/$url/" "/config/apache.conf"
+
+echo "$(tput setaf 2)We are done! Ready to start. Please run start.sh!$(tput sgr 0)"
